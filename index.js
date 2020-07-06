614aa0f88adde5c84939fccb4ff1d07cd13fb25b
Felix <felix.julian.arnold@gmail.com>
2020-07-06 16:49:11 +0200

    add deepai, add @mention event

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 require('dotenv').config();
 const { App } = require('@slack/bolt');
 const store = require('./store');
+const deepai = require('deepai'); // OR include deepai.min.js as a script tag in your HTML
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN
 });
 
+app.event('app_mention', ({ event, say }) => {
+  // Look up the user from DB
+  let user = store.getUser(event.user);
+
+  deepai.setApiKey(process.env.DEEPAI_API_KEY);
+
+  let resp;
+  const text = event.text.replace(/<@[a-zA-Z0-9]*>/g, '');
+
+  if (text === '') {
+    say('@ me with some words!')
+  } else {
+    async function getImage() {
+      resp = await deepai.callStandardApi("text2img", {
+        text: text,
+      });
+      console.log(resp.output_url);
+    }
+
+    function postImage() {
+      say({
+        "attachments": [
+          {
+            // "text": "Optional text that appears within the attachment",
+            "image_url": resp.output_url,
+          }
+        ]
+      });
+    }
+
+    getImage().then(postImage);
+  }
+});
 
 app.event('app_home_opened', ({ event, say }) => {
   // Look up the user from DB
@@ -25,8 +59,7 @@ app.event('app_home_opened', ({ event, say }) => {
   }
 });
 
-app.command('/hi', async ({ message, event, action, command, options }) => {
-  say('Hi')
+app.event('app_mention', (payload) => {
 });
 
 // Start your app
